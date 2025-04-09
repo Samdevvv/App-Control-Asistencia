321bd68df45b7f8fb101b2250a61799f61e1228a
Samdevvv <xamuelacostasiri@gmail.com>
2025-04-09 11:04:45 -0400

    CambiosLogin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,15 @@
+import React from "react";
+import RegistroSucursal from "./componentes/RegistroSucursal"
+import RegistroUsuario from "./componentes/RegistroUsuarios"
+import Login from "./componentes/Login"
+
+
 function App() {
-  return <p>"Hola mundo"</p>;
+  return (
+    <div>
+  <Login/>
+    </div>
+  );
 }
 
 export default App;
